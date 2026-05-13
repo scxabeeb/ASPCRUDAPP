9eb0d5b31503a967b3781674e31d7d3671e209ba
saed habeb <scxabeeb@gmail.com>
2026-05-13 11:40:15 +0300

    Changes

--- obj/Debug/net9.0/week1.AssemblyInfo.cs
+++ obj/Debug/net9.0/week1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c05e1b8457d3c03223e871bd90ecbd52635a49")]
 [assembly: System.Reflection.AssemblyProductAttribute("week1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
